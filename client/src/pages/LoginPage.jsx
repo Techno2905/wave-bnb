0e2f727573bbaad0fa40a71e1d47523bb033046b
Techno2905 <kishore29@null.net>
2023-06-10 11:37:49 +0530

    update-login-required

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,47 +1,61 @@
 import axios from "axios";
-import { useContext, useState } from "react"
-import { Link, Navigate } from "react-router-dom"
+import { useContext, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
 import { UserContext } from "../UserContext";
-export default function LoginPage(){
-const [email,setEmail]= useState('');
-const [password,setPassword]= useState('');
-const [redirect,setRedirect] = useState(null);
-const {setUser} = useContext(UserContext);
+export default function LoginPage() {
+     const [email, setEmail] = useState("");
+     const [password, setPassword] = useState("");
+     const [redirect, setRedirect] = useState(null);
+     const { setUser } = useContext(UserContext);
 
-    async function handleLoginSubmit(ev) {
-        ev.preventDefault();
-        try {
-        const { data } = await axios.post('/login', { email, password });
-        setUser(data);
-        setRedirect('/');
-        alert('Login successful');
-        } catch (error) {
-        alert('Login failed');
-        }
-    }
-  
+     async function handleLoginSubmit(ev) {
+          ev.preventDefault();
+          try {
+               const { data } = await axios.post("/login", { email, password });
+               setUser(data);
+               setRedirect("/");
+               alert("Login successful");
+          } catch (error) {
+               alert("Login failed");
+          }
+     }
 
-    if(redirect){
-        return <Navigate to={redirect}/>
-    }
+     if (redirect) {
+          return <Navigate to={redirect} />;
+     }
 
-    return (
-        <div className="mt-4 grow flex items-center justify-around">
-           <div className="mb-64">
-            <h1 className="text-4xl text-center mb-4">Login</h1>
-            <form action="" className="max-w-md mx-auto " onSubmit={handleLoginSubmit}>
-                <input type="email" 
-                    placeholder="your@email.com" 
-                    value={email} onChange={ev=>setEmail(ev.target.value)} />
-                <input type="password" 
-                    placeholder="password" 
-                    value={password} onChange={ev=>setPassword(ev.target.value)}/>
-                <button className="primary font-bold">Login</button>
-                <div className="text-center py-2 text-gray-500">
-                Don't have an account? <Link to={'/register'} className="underline text">Register now!</Link>
-                </div>
-            </form>
-            </div>
-        </div>
-    )
+     return (
+          <div className="mt-4 grow flex items-center justify-around">
+               <div className="mb-64">
+                    <h1 className="text-4xl text-center mb-4">Login</h1>
+                    <form
+                         action=""
+                         className="max-w-md mx-auto "
+                         onSubmit={handleLoginSubmit}
+                    >
+                         <input
+                              type="email"
+                              placeholder="your@email.com"
+                              value={email}
+                              required
+                              onChange={(ev) => setEmail(ev.target.value)}
+                         />
+                         <input
+                              type="password"
+                              placeholder="password"
+                              value={password}
+                              required
+                              onChange={(ev) => setPassword(ev.target.value)}
+                         />
+                         <button className="primary font-bold">Login</button>
+                         <div className="text-center py-2 text-gray-500">
+                              Don't have an account?{" "}
+                              <Link to={"/register"} className="underline text">
+                                   Register now!
+                              </Link>
+                         </div>
+                    </form>
+               </div>
+          </div>
+     );
 }
