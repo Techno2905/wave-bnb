9f3fa2f141eb31a929ae812cbc4b05cc6e6405a5
Techno2905 <kishore29@null.net>
2023-06-17 21:46:31 +0530

    Update Header.jsx

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -6,7 +6,7 @@ import Icon from "./assets/icon.jsx";
 export default function Header() {
      const { user } = useContext(UserContext);
      return (
-          <header className=" flex justify-between">
+          <header className=" flex lg:justify-between sm:items-start ">
                <Link to={"/"} className="flex items-center gap-1">
                     <Icon />
                     <span className="font-bold text-xl">wavebnb</span>
@@ -37,25 +37,11 @@ export default function Header() {
                     </div>
                </div>
                {/*User Login Button*/}
-               <Link
-                    to={user ? "/account" : "/login"}
-                    className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 items-center"
-               >
-                    <svg
-                         xmlns="http://www.w3.org/2000/svg"
-                         fill="none"
-                         viewBox="0 0 24 24"
-                         strokeWidth={1.5}
-                         stroke="currentColor"
-                         className="w-6 h-6"
+               <div>
+                    <Link
+                         to={user ? "/account" : "/login"}
+                         className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 items-center "
                     >
-                         <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                         />
-                    </svg>
-                    <div>
                          <svg
                               xmlns="http://www.w3.org/2000/svg"
                               fill="none"
@@ -67,12 +53,28 @@ export default function Header() {
                               <path
                                    strokeLinecap="round"
                                    strokeLinejoin="round"
-                                   d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                                   d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                               />
                          </svg>
-                    </div>
-                    {!!user && <div>{user.name}</div>}
-               </Link>
+                         <div>
+                              <svg
+                                   xmlns="http://www.w3.org/2000/svg"
+                                   fill="none"
+                                   viewBox="0 0 24 24"
+                                   strokeWidth={1.5}
+                                   stroke="currentColor"
+                                   className="w-6 h-6"
+                              >
+                                   <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                                   />
+                              </svg>
+                         </div>
+                         {!!user && <div>{user.name}</div>}
+                    </Link>
+               </div>
           </header>
      );
 }
