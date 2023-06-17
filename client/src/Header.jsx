6be2fc9611b6401236e4d89c8a46bde293d56b48
Techno2905 <kishore29@null.net>
2023-06-17 21:54:42 +0530

    Update Header.jsx

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -6,38 +6,11 @@ import Icon from "./assets/icon.jsx";
 export default function Header() {
      const { user } = useContext(UserContext);
      return (
-          <header className=" flex justify-between  ">
+          <header className=" flex justify-between ">
                <Link to={"/"} className="flex items-center gap-1">
                     <Icon />
                     <span className="font-bold text-xl">wavebnb</span>
                </Link>
-               <div className="invisible lg:visible md:visible">
-                    <div className="flex gap-2 border border-gray-300 rounded-full py-1 px-4 shadow-md shadow-gray-300 items-center ">
-                         <div>Anywhere</div>
-                         <div className="border border-l border-gray-300 h-full mx-2"></div>
-                         <div>Any week</div>
-                         <div className="border border-l border-gray-300 h-full mx-2"></div>
-                         <div>Add guests</div>
-                         <button className="shadow bg-primary text-white p-1.5 rounded-full ">
-                              <svg
-                                   xmlns="http://www.w3.org/2000/svg"
-                                   fill="none"
-                                   viewBox="0 0 24 24"
-                                   strokeWidth={1.5}
-                                   stroke="currentColor"
-                                   className="w-5 h-5"
-                              >
-                                   <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                                   />
-                              </svg>
-                         </button>
-                    </div>
-               </div>
-               {/*User Login Button*/}
-
                <Link
                     to={user ? "/account" : "/login"}
                     className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 items-center "
